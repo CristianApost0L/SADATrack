c4a209214e95d601a61710aea8260cbbc2341dc2
Cristian Di Iorio <112891194+cristiandiiorio@users.noreply.github.com>
2026-02-05 09:56:54 +0100

    Update __init__.py

--- constants/__init__.py
+++ constants/__init__.py
@@ -25,4 +25,4 @@ BATCH_SIZE = 12
 
 TEMP_CLIP_DIR = "/kaggle/working/temp_processing_clips"
 PROCESSED_CLIP_DIR = "/kaggle/working/output_videos/clips"
-CLIP_DURATION = 60 #seconds
+CLIP_DURATION = 180 #seconds
